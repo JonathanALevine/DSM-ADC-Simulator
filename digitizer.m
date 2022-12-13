fdd7d85b1adc08dd00f7b634d6efb09d5ef56394
Jonathan Levine <jonathanaaron.levine@gmail.com>
2022-12-13 17:36:31 -0500

    fixed digitizer

--- digitizer.m
+++ digitizer.m
@@ -1,10 +1,10 @@
 function [analog_val] = digitizer(digital_val)
     if digital_val == "00"
-        analog_val = -2;
+        analog_val = 0;
     elseif digital_val == "01"
-        analog_val = -2/3;
+        analog_val = 2/3;
     elseif digital_val == "10"
-        analog_val = +2/3;
+        analog_val = 4/3;
     else
         analog_val = 2;
     end
